44cdda2f35d32d0fc623d6b648d7fd41a70375d4
cweng701 <cweng701@usc.edu>
2025-09-29 00:55:50 -0700

    test commit 1

--- dataset/sram_1.tcl
+++ dataset/sram_1.tcl
@@ -1,5 +1,5 @@
 create_inst sram0 SRAM1Kx32
 create_inst sram1 SRAM1Kx32
-moveInst sram0 200 400
+moveInst sram0 100 200
 moveInst sram1 240 400
 place_opt_design
--- dataset/sram_3.tcl
+++ dataset/sram_3.tcl
@@ -1,6 +1,6 @@
 create_inst sram0 SRAM1Kx32
 create_inst sram1 SRAM1Kx32
 moveInst sram0 100 200
-moveInst sram1 500 400
+moveInst sram1 250 200
 fixInst sram0
 fixInst sram1
